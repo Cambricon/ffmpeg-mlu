c46646a82e7470bac641869377cd7ba00a2ecd84
zhuxiaoqiang <zhuxiaoqiang@cambricon.com>
2020-04-20 19:40:43 +0800

    feat(dec&enc):ffmpeg-mlu official release version 1.0.0

---
+++ README.md
@@ -0,0 +1,177 @@
+EN|[CN](README_cn.md)
+
+Cambricon<sup>®</sup> FFmpeg-MLU
+====================================
+
+Cambricon<sup>®</sup> FFmpeg-MLU supports hardware-accelerated video decoding and encoding using a plain C API on Cambricon MLU hardware platforms.
+
+## Requirements ## 
+
+- Supported OS: 
+    - Ubuntu
+	- Centos
+	- Debian
+- Cambricon MLU Driver: 
+    - neuware-mlu270-driver-4.0.3 or later.
+- Cambricon MLU SDK: 
+    - neuware-mlu270-1.2.5-1 or later.
+	
+## Patch and Build FFmpeg-MLU ## 
+
+1. Get FFmpeg sources and patch with the following Git* command:
+
+   ```sh
+   git clone https://gitee.com/mirrors/ffmpeg.git -b release/4.2 --depth=1
+   cd ffmpeg    
+   git apply ../ffmpeg4.2_mlu.patch
+   ```
+2. If you are using CentOS, you need to set ``LD_LIBRARY_PATH`` to the proper directory by running the following commands:
+
+   ```sh
+   export LD_LIBRARY_PATH=/usr/local/neuware/lib64
+   ```
+
+3. Run the following commands to configure and build FFmpeg-MLU:
+
+   ```sh
+   ./configure --enable-gpl \
+               --enable-version3 \
+               --enable-mlumpp \
+               --extra-cflags="-I/usr/local/neuware/include" \
+               --extra-ldflags="-L/usr/local/neuware/lib64" \
+               --extra-libs="-lcncodec -lcnrt -ldl"
+   make -j
+   ```
+4. If you need a MLU transcode demo with multi-threads, run the following commands:
+
+   ```sh
+   make -j examples
+   ```
+
+5. (Optional) If you need support downscaling with MLU operators, build ``mluop_plugin`` and copy ``libeasyOP.so``  to the ``/usr/local/neuware/lib64`` folder.
+
+
+## Decoding and Encoding with FFmpeg-MLU ## 
+
+### Decoding ###
+
+Supported video decoding formats are as follows:
+
+- H.264/AVC 
+    - Codec Name: ``h264_mludec``
+- HEVC
+    - Codec Name: ``hevc_mludec``
+- VP8
+    - Codec Name: ``vp8_mludec``
+- VP9
+    - Codec Name: ``vp9_mludec``
+- JPEG
+    - Codec Name: ``mjpeg_mludec``
+  
+**Example**
+
+```sh
+export LD_LIBRARY_PATH=/usr/local/neuware/lib64
+./ffmpeg -c:v h264_mludec -i input_file -f null -
+```
+### Encoding ###
+
+Supported video encoding formats are as follows:
+
+- H.264/AVC
+    - Codec Name: ``h264_mluenc``
+- HEVC
+    - Codec Name: ``hevc_mluenc``
+
+**Example**  
+
+```sh
+export LD_LIBRARY_PATH=/usr/local/neuware/lib64
+./ffmpeg -i input_file -c:v h264_mluenc <output.h264>
+```
+## Basic Testing ##
+
+### Encode Baseline Test ###
+
+
+```sh
+./ffmpeg -benchmark -re -i input.mkv -c:v h264_mluenc -f null -
+```
+### Decode with Scaling ###
+```sh
+./ffmpeg -y -vsync 0 -c:v h264_mludec -i input_1920x1080.mkv -vf scale=320:240 output_320x240.mp4
+```
+### 1:1 Transcode Without Scaling ###
+
+    ./ffmpeg -y -vsync 0 -c:v h264_mludec -i fhd_input.mkv -c:a copy -c:v h264_mluenc -b:v 5M output.mp4
+
+### 1:N Transcode With Scaling ###
+```sh
+./ffmpeg -y -vsync 0 -c:v h264_mludec -i fhd_input.mkv -vf scale=1280:720 -c:a copy -c:v h264_mluenc -b:v 2M output1.mp4 -vf scale=640:360 -c:a copy -c:v h264_mluenc -b:v 512K output2.mp4
+```
+## Quality Testing ##
+
+This section introduces how to test video quality with PSNR and SSTM.
+
+### PSNR ###
+
+```sh
+./ffmpeg -i src.h264  -i dst.h264  -lavfi psnr="stats_file=psnr.log" -f null -
+```
+### SSIM ###
+
+```sh
+./ffmpeg -i src.h264  -i dst.h264  -lavfi ssim="stats_file=ssim.log" -f null -
+```
+## Performance Fine-Tune ##
+
+This section introduces how to improve the performance.
+
+### MLU Decoder ###
+
+
+|Option name|Type|Description|
+|-|-|-|
+|device_id|int|Select the accelerator card. <br>Supported values range from **0** to *INT_MAX*. *INT_MAX* is the total number of accelerator cards minus 1. <br>The default value is **0**.|
+|instance_id|int|Select the VPU instance. <br>Supported values are: <br>- Value in the range **0** - **5**: Represents VPU instance. <br>- **6**: The VPU instance is auto-selected. <br>The default value is **6**.|
+|cnrt_init_flag|int|Initialize or destory cnrt context in FFmpeg. <br>Supported values are: <br>- **0**: Represents disabled. <br>- **1**: Represents enabled. <br>The default value is **1**.|
+|input_buf_num|int|Number of input buffers for decoder. <br>Supported values range from **1** to **32**. <br>The default value is **5**.|
+|output_buf_num|int|Number of output buffers for decoder. <br>Supported values range from **1** to **32**. <br>The default value is **6**.|
+|stride_align|int|Stride align of output buffers for decoder. <br>Supported values range from **1** to **128**, can be **2^(0 - 7)**. <br>The default value is **1**.|
+|output_pixfmt|int|The output pixel format. <br>Supported values are: <br>- **0**: Represents NV12. <br>- **1**: Represents NV21. <br>- **2**: Represents I420. <br>The default value is **0**.|
+|resize|string|Resize (width)x(height). <br>Only supports **1/2** and **1/4** for down scaling. <br>The default is null.|
+|trace|int|MLU FFmpeg MLU trace switch. <br>Supported values are: <br>- **0**: Represents disabled. <br>- **1**: Represents enabled. <br>The default value is **0**.|
+
+### MLU Encoder ###
+
+#### Common ###
+
+|Option name|Type|Description|
+|-|-|-|
+|device_id|int|Select the accelerator card. <br>Supported values range from **0** to *INT_MAX*. *INT_MAX* is the total number of accelerator cards minus 1. <br>The default value is **0**.|
+|instance_id|int|Select the VPU instance. <br>Supported values are: <br>- Value in the range **0** - **1**: Represents VPU instance. <br>- **6**: The VPU instance is auto-selected. <br>The default value is **6**.|
+|cnrt_init_flag|int|Initialize or destory cnrt context in FFmpeg. <br>Supported values are: <br>- **0**: Represents disabled. <br>- **1**: Represents enabled. <br>The default value is **1**.|
+|input_buf_num|int|Number of input buffers for encoder. <br>Supported values range from **1** to **32**. <br>The default value is **3**.|
+|output_buf_num|int|Number of output buffers for encoder. <br>Supported values range from **1** to **32**. <br>The default value is **5**.|
+|trace|int|MLU FFmpeg debug switch. <br>Supported values are: <br>- **0** Represents disabled.<br>- **1**: Represents enabled. <br>The default value is **0**.|
+|init_qpP|int|Initial QP value for P frame, set P frame QP. <br>Supported values range from **-1** to **51**. <br>The default value is **-1**.|
+|init_qpI|int|Initial QP value for I frame, set I frame QP. <br>Supported values range from **-1** to **51**. <br>The default value is **-1**.|
+|init_qpB|int|Initial QP value for B frame, set B frame QP. <br>Supported values range from **-1** to **51**. <br>The default value is **-1**.|
+|qp|int|Constant QP rate control method, same as FFmpeg cqp. <br>Supported values range from **-1** to **51**. <br>The default value is **-1**.|
+|vbr_minqp|int|Variable bitrate mode with MinQP, same as FFmepg qmin. <br>Supported values range from **-1** to **51**. <br>The default value is **-1**.|
+|vbr_maxqp|int|Variable bitrate mode with MaxQP, same as FFmpeg qmax. <br>Supported values range from **-1** to **51**. <br>The default value is **-1**.|
+
+#### H264 ###
+
+|Option name|Type|Description|
+|-|-|-|
+|profile|const|Set the encoding profile. <br>Supported values are: **baseline**, **main**, **high**, and **high444p**. <br>The default value is **high**.|
+|level|const|Set the encoding level restriction. <br>Supported values are: in the range **1** - **5.1**, or **auto**. <br>The default value is **4.2**.|
+|coder|const|Set the encoding entropy mode. <br>Supported values are: **cabac** and **cavlc**. <br>The default value is **cavlc**.|
+
+#### HEVC ###
+
+|Option name|Type|Description|
+|-|-|-|
+|profile|const|Set the encoding profile. <br>Supported values are: **main**, **main_still**, **main_intra**, and **main10**. <br>The default value is **main**.|
+|level|const|Set the encoding level restriction. <br>Supported values are: in the range **1** - **6.2**, or **auto**. <br>The default value is **1**.|
---
+++ mluop_plugin/src/yuv2yuv_resize.cpp
@@ -0,0 +1,511 @@
+/*************************************************************************
+ * Copyright (C) [2019] by Cambricon, Inc. All rights reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *************************************************************************/
+#include <chrono>
+#include <ctime>
+#include <deque>
+#include <iostream>
+#include <string>
+#include <sys/time.h>
+
+#include "cnml.h"
+#include "cnplugin.h"
+#include "cnrt.h"
+#include "mlu_loging.hpp"
+#include "mluop_plugin.h"
+
+#define PRINT_TIME 0
+
+inline bool cnrtCheck(cnrtRet_t cnrtret, std::string *estr,
+                      const std::string &msg) {
+  if (CNRT_RET_SUCCESS != cnrtret) {
+    *estr = "CNRT " + msg + " ERRCODE:" + std::to_string(cnrtret);
+    return false;
+  }
+  return true;
+}
+inline bool cnmlCheck(cnmlStatus_t cnmlret, std::string *estr,
+                      const std::string &msg) {
+  if (CNML_STATUS_SUCCESS != cnmlret) {
+    *estr = "CNML " + msg + " ERRCODE:" + std::to_string(cnmlret);
+    return false;
+  }
+  return true;
+}
+
+using std::string;
+using std::to_string;
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// internal functions
+struct ResizeYuv2Yuv {
+  void **input_addrs = nullptr;
+  void **output_addrs = nullptr;
+  cnmlPluginResizeAndColorCvtParam_t param = nullptr;
+  cnmlBaseOp_t op = nullptr;
+  cnmlTensor_t *cnml_input_ptr = nullptr;
+  cnmlTensor_t *cnml_output_ptr = nullptr;
+  // enum
+  int batch_size = 1;
+  cnmlCoreVersion_t version = CNML_MLU270;
+#if PRINT_TIME
+  cnrtNotifier_t event_begin = nullptr;
+  cnrtNotifier_t event_end = nullptr;
+#endif
+};
+
+static bool createParam(ResizeYuv2Yuv *yuv2yuv, cnmlCoreVersion_t version,
+                        int src_w, int src_h, int dst_w, int dst_h,
+                        int batch_size, string *estr) {
+  if (!yuv2yuv) {
+    *estr = "[CreateParam] yuv2yuv is nullptr";
+    return false;
+  }
+
+  yuv2yuv->batch_size = batch_size;
+  yuv2yuv->version = version;
+  ioParams mode;
+  auto cnmlret = cnmlCreatePluginResizeYuvToYuvOpParam(
+      &(yuv2yuv->param), src_h, src_w, dst_h, dst_w, mode, yuv2yuv->version);
+
+  if (!cnmlCheck(cnmlret, estr,
+                 "Create Plugin ResizeYuv2Yuv Op param failed.")) {
+    return false;
+  }
+  return true;
+}
+
+void freeTensorPtr(ResizeYuv2Yuv *yuv2yuv) {
+  if (yuv2yuv) {
+    if (yuv2yuv->cnml_input_ptr) {
+      free(yuv2yuv->cnml_input_ptr);
+    }
+    if (yuv2yuv->cnml_output_ptr) {
+      free(yuv2yuv->cnml_output_ptr);
+    }
+  }
+}
+
+static void initTensorPtr(ResizeYuv2Yuv *yuv2yuv) {
+  if (yuv2yuv) {
+    auto param = yuv2yuv->param;
+    yuv2yuv->cnml_input_ptr = reinterpret_cast<cnmlTensor_t *>(
+        malloc(sizeof(cnmlTensor_t) * param->input_num));
+    yuv2yuv->cnml_output_ptr = reinterpret_cast<cnmlTensor_t *>(
+        malloc(sizeof(cnmlTensor_t) * param->output_num));
+  }
+}
+
+static bool destroyTensor(ResizeYuv2Yuv *yuv2yuv, string *estr) {
+  if (!yuv2yuv) {
+    *estr = "[DestroyTensor] yuv2yuv is nullptr";
+    return false;
+  }
+  auto param = yuv2yuv->param;
+  bool success = true;
+  for (int i = 0; i < param->input_num; i++) {
+    if (yuv2yuv->cnml_input_ptr[i]) {
+      success = cnmlCheck(cnmlDestroyTensor(&yuv2yuv->cnml_input_ptr[i]), estr,
+                          "Destroy input Tensor failed.");
+    }
+  }
+  for (int i = 0; i < param->output_num; i++) {
+    if (yuv2yuv->cnml_output_ptr[i]) {
+      success = cnmlCheck(cnmlDestroyTensor(&yuv2yuv->cnml_output_ptr[i]), estr,
+                          "Destroy output Tensor failed.");
+    }
+  }
+  return success;
+}
+
+static bool createTensor(ResizeYuv2Yuv *yuv2yuv, string *estr) {
+  if (!yuv2yuv) {
+    *estr = "[CreateTensor] yuv2yuv is nullptr";
+    return false;
+  }
+  auto param = yuv2yuv->param;
+
+  if (param->input_num != 2 || param->output_num != 2) {
+    *estr = "Input number or output number is not 2. Input num: " +
+            to_string(param->input_num) +
+            " Output num: " + to_string(param->input_num);
+    return false;
+  }
+
+  int shape[4] = {yuv2yuv->batch_size, 1, 1, 1};
+  cnmlDataType_t dt = CNML_DATA_INT32;
+
+  for (uint8_t i = 0; i < param->input_num; i++) {
+    // input tensor
+    auto cnmlret =
+        cnmlCreateTensor_V2(&(yuv2yuv->cnml_input_ptr[i]), CNML_TENSOR);
+    if (!cnmlCheck(cnmlret, estr, "Create input tensor failed."))
+      return false;
+    cnmlret = cnmlSetTensorShape(yuv2yuv->cnml_input_ptr[i], 4, shape);
+    if (!cnmlCheck(cnmlret, estr, "Set input tensor shape failed."))
+      return false;
+    cnmlret = cnmlSetTensorDataType(yuv2yuv->cnml_input_ptr[i], dt);
+    if (!cnmlCheck(cnmlret, estr, "Set input tensor data type failed."))
+      return false;
+    // output tensor
+    cnmlret = cnmlCreateTensor_V2(&(yuv2yuv->cnml_output_ptr[i]), CNML_TENSOR);
+    if (!cnmlCheck(cnmlret, estr, "Create output tensor failed."))
+      return false;
+    cnmlret = cnmlSetTensorShape(yuv2yuv->cnml_output_ptr[i], 4, shape);
+    if (!cnmlCheck(cnmlret, estr, "Set output tensor shape failed."))
+      return false;
+    cnmlret = cnmlSetTensorDataType(yuv2yuv->cnml_output_ptr[i], dt);
+    if (!cnmlCheck(cnmlret, estr, "Set output tensor shape failed."))
+      return false;
+  }
+
+  return true;
+}
+
+static bool createAndCompileOp(const int &core_limit, ResizeYuv2Yuv *yuv2yuv,
+                               string *estr) {
+  if (!yuv2yuv) {
+    *estr = "[CreateAndCompileOp] yuv2yuv is nullptr";
+    return false;
+  }
+  auto param = yuv2yuv->param;
+  initTensorPtr(yuv2yuv);
+  if (!createTensor(yuv2yuv, estr)) {
+    return false;
+  }
+  auto cnmlret = cnmlCreatePluginResizeYuvToYuvOp(
+      &(yuv2yuv->op), param, yuv2yuv->cnml_input_ptr, yuv2yuv->cnml_output_ptr);
+  if (!cnmlCheck(cnmlret, estr, "Create Plugin ResizeYuvToYuv Op failed."))
+    return false;
+
+  cnmlret = cnmlCompileBaseOp(yuv2yuv->op, yuv2yuv->version, core_limit);
+  if (!cnmlCheck(cnmlret, estr, "Compile Plugin ResizeYuvToYuv Op failed."))
+    return false;
+
+  return true;
+}
+
+static void freeIoAddrsPtr(ResizeYuv2Yuv *yuv2yuv) {
+  if (yuv2yuv) {
+    if (yuv2yuv->input_addrs) {
+      free(yuv2yuv->input_addrs);
+    }
+    if (yuv2yuv->output_addrs) {
+      free(yuv2yuv->output_addrs);
+    }
+  }
+}
+
+static void initIOAddrsPtr(ResizeYuv2Yuv *yuv2yuv) {
+  if (yuv2yuv) {
+    yuv2yuv->input_addrs = reinterpret_cast<void **>(
+        malloc(sizeof(void *) * yuv2yuv->param->input_num));
+    yuv2yuv->output_addrs = reinterpret_cast<void **>(
+        malloc(sizeof(void *) * yuv2yuv->param->output_num));
+  }
+}
+
+static bool destroyResizeYuv2Yuv(ResizeYuv2Yuv *yuv2yuv, string *estr) {
+  bool success = true;
+  if (yuv2yuv) {
+#if PRINT_TIME
+    if (yuv2yuv->event_begin) {
+      success = cnrtCheck(cnrtDestroyNotifier(&yuv2yuv->event_begin), estr,
+                          "Destroy event begin failed.");
+    }
+    if (yuv2yuv->event_end) {
+      success = cnrtCheck(cnrtDestroyNotifier(&yuv2yuv->event_end), estr,
+                          "Destroy event end failed.");
+    }
+#endif
+    success = destroyTensor(yuv2yuv, estr);
+    freeTensorPtr(yuv2yuv);
+
+    freeIoAddrsPtr(yuv2yuv);
+
+    if (yuv2yuv->op) {
+      success = cnmlCheck(cnmlDestroyBaseOp(&(yuv2yuv->op)), estr,
+                          "Destroy resize yuv2yuv op failed.");
+    }
+    if (yuv2yuv->param) {
+      success =
+          cnmlCheck(cnmlDestroyPluginResizeYuvToYuvOpParam(&yuv2yuv->param),
+                    estr, "Destroy resize yuv2yuv param failed.");
+    }
+    delete yuv2yuv;
+  }
+  success = cnmlCheck(cnmlExit(), estr, "Exit failed.");
+  return success;
+}
+
+static bool createResizeYuv2Yuv(ResizeYuv2Yuv **yuv2yuv_ptr, int core_num,
+                                cnmlCoreVersion_t version, int src_w, int src_h,
+                                int dst_w, int dst_h, int batch_size,
+                                string *estr) {
+  (*yuv2yuv_ptr) = new ResizeYuv2Yuv;
+
+  if (!*yuv2yuv_ptr) {
+    *estr = "Create ResizeYuv2Yuv pointer failed";
+    return false;
+  }
+
+  if (!cnmlCheck(cnmlInit(0), estr, "Init failed"))
+    return false;
+
+#if PRINT_TIME
+  if (!cnrtCheck(cnrtCreateNotifier(&(*yuv2yuv_ptr)->event_begin), estr,
+                 "create notifier event_begin failed.")) {
+    return false;
+  }
+  if (!cnrtCheck(cnrtCreateNotifier(&(*yuv2yuv_ptr)->event_end), estr,
+                 "create notifier event_end failed.")) {
+    return false;
+  }
+#endif
+
+  if (!createParam(*yuv2yuv_ptr, version, src_w, src_h, dst_w, dst_h,
+                   batch_size, estr))
+    return false;
+  if (!createAndCompileOp(core_num, *yuv2yuv_ptr, estr))
+    return false;
+
+  initIOAddrsPtr(*yuv2yuv_ptr);
+  return true;
+}
+
+static bool computeResizeYuv2Yuv(ResizeYuv2Yuv *yuv2yuv, void *dst_y,
+                                 void *dst_uv, void *src_y, void *src_uv,
+                                 cnrtQueue_t queue, string *estr) {
+  if (!yuv2yuv) {
+    *estr = "[ComputeResizeYuv2Yuv] yuv2yuv is nullptr";
+    return false;
+  }
+  yuv2yuv->input_addrs[0] = src_y;
+  yuv2yuv->input_addrs[1] = src_uv;
+  yuv2yuv->output_addrs[0] = dst_y;
+  yuv2yuv->output_addrs[1] = dst_uv;
+#if PRINT_TIME
+  cnrtPlaceNotifier(yuv2yuv->event_begin, queue);
+  auto start_tp = std::chrono::high_resolution_clock::now();
+#endif
+  auto cnmlret = cnmlComputePluginResizeYuvToYuvOpForward(
+      yuv2yuv->op, yuv2yuv->param, yuv2yuv->cnml_input_ptr,
+      yuv2yuv->input_addrs, yuv2yuv->cnml_output_ptr, yuv2yuv->output_addrs,
+      queue);
+  if (!cnmlCheck(cnmlret, estr, "Compute Plugin ResizeYuv2Yuv Op failed.")) {
+    return false;
+  }
+#if PRINT_TIME
+  cnrtPlaceNotifier(yuv2yuv->event_end, queue);
+#endif
+  // sync queue
+  int success = cnrtCheck(cnrtSyncQueue(queue), estr, "Sync queue failed.");
+#if PRINT_TIME
+  auto end_tp = std::chrono::high_resolution_clock::now();
+  float hw_time = 0.f;
+  cnrtNotifierDuration(yuv2yuv->event_begin, yuv2yuv->event_end, &hw_time);
+  std::cout << "hardware " << hw_time / 1000.f << "ms" << std::endl;
+  std::chrono::duration<double, std::milli> diff = end_tp - start_tp;
+  std::cout << "software " << diff.count() << "ms" << std::endl;
+#endif
+  return success;
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+// external supports
+typedef struct {
+  cnrtQueue_t queue_ = nullptr;
+  ResizeYuv2Yuv *yuv2yuv_ = nullptr;
+  std::deque<std::pair<void *, void *>> src_yuv_ptrs_cache_;
+  std::deque<std::pair<void *, void *>> dst_yuv_ptrs_cache_;
+  void *src_y_ptrs_cpu_ = nullptr, *src_uv_ptrs_cpu_ = nullptr;
+  void *src_y_ptrs_mlu_ = nullptr, *src_uv_ptrs_mlu_ = nullptr;
+  void *dst_y_ptrs_cpu_ = nullptr, *dst_uv_ptrs_cpu_ = nullptr;
+  void *dst_y_ptrs_mlu_ = nullptr, *dst_uv_ptrs_mlu_ = nullptr;
+  std::string estr_;
+  int batch_size_;
+} Yuv2YuvResizeContext_t;
+
+int mluop_resize_yuv_init(HANDLE *h, int input_w, int input_h,
+                          int input_stride_in_bytes, int output_w, int output_h,
+                          int output_stride_in_bytes, int device_id) {
+  Yuv2YuvResizeContext_t *ctx = new Yuv2YuvResizeContext_t;
+  cnrtCreateQueue(&ctx->queue_);
+  ctx->batch_size_ = 1;
+  // malloc cpu
+  ctx->src_y_ptrs_cpu_ = malloc(sizeof(void *) * ctx->batch_size_);
+  ctx->src_uv_ptrs_cpu_ = malloc(sizeof(void *) * ctx->batch_size_);
+  ctx->dst_y_ptrs_cpu_ = malloc(sizeof(void *) * ctx->batch_size_);
+  ctx->dst_uv_ptrs_cpu_ = malloc(sizeof(void *) * ctx->batch_size_);
+  // malloc mlu
+  cnrtRet_t cnret =
+      cnrtMalloc(&ctx->src_y_ptrs_mlu_, sizeof(void *) * ctx->batch_size_);
+  if (!cnrtCheck(cnret, &ctx->estr_, "Malloc src y mlu buffer failed."))
+    return false;
+  cnret = cnrtMalloc(&ctx->src_uv_ptrs_mlu_, sizeof(void *) * ctx->batch_size_);
+  if (!cnrtCheck(cnret, &ctx->estr_, "Malloc src uv mlu buffer failed."))
+    return false;
+  cnret = cnrtMalloc(&ctx->dst_y_ptrs_mlu_, sizeof(void *) * ctx->batch_size_);
+  if (!cnrtCheck(cnret, &ctx->estr_, "Malloc dst y mlu buffer failed."))
+    return false;
+  cnret = cnrtMalloc(&ctx->dst_uv_ptrs_mlu_, sizeof(void *) * ctx->batch_size_);
+  if (!cnrtCheck(cnret, &ctx->estr_, "Malloc dst uv mlu buffer failed."))
+    return false;
+
+  cnrtDeviceInfo_t info;
+  bool success = false;
+  int dev_id = 0;
+  if (device_id > 0) {
+    dev_id = device_id;
+  }
+  cnret = cnrtGetDeviceInfo(&info, dev_id);
+  switch (info.core_version) {
+  case CNRT_MLU270:
+    success =
+        createResizeYuv2Yuv(&ctx->yuv2yuv_, 4, CNML_MLU270, input_w, input_h,
+                            output_w, output_h, ctx->batch_size_, &ctx->estr_);
+    break;
+  case CNRT_MLU220:
+    success =
+        createResizeYuv2Yuv(&ctx->yuv2yuv_, 4, CNML_MLU220, input_w, input_h,
+                            output_w, output_h, ctx->batch_size_, &ctx->estr_);
+    break;
+  default:
+    logging::ERROR("Not support this case");
+  }
+  if (!success) {
+    std::cout << ctx->estr_ << std::endl;
+    if (ctx->yuv2yuv_) {
+      if (!::destroyResizeYuv2Yuv(ctx->yuv2yuv_, &ctx->estr_)) {
+        logging::ERROR("DestroyResizeYuv2Yuv Error: " + ctx->estr_);
+      }
+      ctx->yuv2yuv_ = nullptr;
+    }
+  }
+  *h = static_cast<void *>(ctx);
+  return success;
+}
+
+int mluop_resize_yuv_exec(HANDLE h, void *input_y, void *input_uv,
+                          void *output_y, void *output_uv) {
+
+  Yuv2YuvResizeContext_t *ctx = static_cast<Yuv2YuvResizeContext_t *>(h);
+  if (nullptr == ctx->queue_) {
+    logging::ERROR("cnrt queue is null.");
+    return -1;
+  }
+  ctx->src_yuv_ptrs_cache_.push_back(std::make_pair(input_y, input_uv));
+  ctx->dst_yuv_ptrs_cache_.push_back(std::make_pair(output_y, output_uv));
+
+  if (static_cast<int>(ctx->src_yuv_ptrs_cache_.size()) < ctx->batch_size_ ||
+      static_cast<int>(ctx->dst_yuv_ptrs_cache_.size()) < ctx->batch_size_) {
+    logging::ERROR("Batchsize is " + std::to_string(ctx->batch_size_) +
+                    ", but only has input: " + std::to_string(ctx->src_yuv_ptrs_cache_.size()) +
+                    ", output: " + std::to_string(ctx->dst_yuv_ptrs_cache_.size()));
+    return false;
+  }
+  for (int bi = 0; bi < ctx->batch_size_; ++bi) {
+    reinterpret_cast<void **>(ctx->src_y_ptrs_cpu_)[bi] =
+        ctx->src_yuv_ptrs_cache_.front().first;
+    reinterpret_cast<void **>(ctx->src_uv_ptrs_cpu_)[bi] =
+        ctx->src_yuv_ptrs_cache_.front().second;
+    reinterpret_cast<void **>(ctx->dst_y_ptrs_cpu_)[bi] =
+        ctx->dst_yuv_ptrs_cache_.front().first;
+    reinterpret_cast<void **>(ctx->dst_uv_ptrs_cpu_)[bi] =
+        ctx->dst_yuv_ptrs_cache_.front().second;
+    ctx->src_yuv_ptrs_cache_.pop_front();
+    ctx->dst_yuv_ptrs_cache_.pop_front();
+  }
+
+  cnrtRet_t cnret = cnrtMemcpy(ctx->src_y_ptrs_mlu_, ctx->src_y_ptrs_cpu_,
+                               sizeof(void *) * ctx->batch_size_,
+                               CNRT_MEM_TRANS_DIR_HOST2DEV);
+  if (!cnrtCheck(cnret, &ctx->estr_,
+                 "Memcpy src y from host to device failed."))
+    return -1;
+  cnret = cnrtMemcpy(ctx->src_uv_ptrs_mlu_, ctx->src_uv_ptrs_cpu_,
+                     sizeof(void *) * ctx->batch_size_,
+                     CNRT_MEM_TRANS_DIR_HOST2DEV);
+  if (!cnrtCheck(cnret, &ctx->estr_,
+                 "Memcpy src uv from host to device failed."))
+    return -1;
+  cnret = cnrtMemcpy(ctx->dst_y_ptrs_mlu_, ctx->dst_y_ptrs_cpu_,
+                     sizeof(void *) * ctx->batch_size_,
+                     CNRT_MEM_TRANS_DIR_HOST2DEV);
+  if (!cnrtCheck(cnret, &ctx->estr_,
+                 "Memcpy dst y from host to device failed."))
+    return -1;
+  cnret = cnrtMemcpy(ctx->dst_uv_ptrs_mlu_, ctx->dst_uv_ptrs_cpu_,
+                     sizeof(void *) * ctx->batch_size_,
+                     CNRT_MEM_TRANS_DIR_HOST2DEV);
+  if (!cnrtCheck(cnret, &ctx->estr_,
+                 "Memcpy dst uv from host to device failed."))
+    return -1;
+  return ::computeResizeYuv2Yuv(
+      ctx->yuv2yuv_, ctx->dst_y_ptrs_mlu_, ctx->dst_uv_ptrs_mlu_,
+      ctx->src_y_ptrs_mlu_, ctx->src_uv_ptrs_mlu_, ctx->queue_, &ctx->estr_);
+}
+int mluop_resize_yuv_destroy(HANDLE h) {
+  Yuv2YuvResizeContext_t *ctx = static_cast<Yuv2YuvResizeContext_t *>(h);
+  if (ctx) {
+    if (ctx->yuv2yuv_) {
+      if (!::destroyResizeYuv2Yuv(ctx->yuv2yuv_, &ctx->estr_)) {
+        logging::ERROR("DestroyResizeYuv2Yuv Error: " + ctx->estr_);
+      }
+      ctx->yuv2yuv_ = nullptr;
+    }
+    if (ctx->src_y_ptrs_cpu_) {
+      free(ctx->src_y_ptrs_cpu_);
+      ctx->src_y_ptrs_cpu_ = nullptr;
+    }
+    if (ctx->src_uv_ptrs_cpu_) {
+      free(ctx->src_uv_ptrs_cpu_);
+      ctx->src_uv_ptrs_cpu_ = nullptr;
+    }
+    if (ctx->dst_y_ptrs_cpu_) {
+      free(ctx->dst_y_ptrs_cpu_);
+      ctx->dst_y_ptrs_cpu_ = nullptr;
+    }
+    if (ctx->dst_uv_ptrs_cpu_) {
+      free(ctx->dst_uv_ptrs_cpu_);
+      ctx->dst_uv_ptrs_cpu_ = nullptr;
+    }
+    if (ctx->src_y_ptrs_mlu_) {
+      cnrtFree(ctx->src_y_ptrs_mlu_);
+      ctx->src_y_ptrs_mlu_ = nullptr;
+    }
+    if (ctx->src_uv_ptrs_mlu_) {
+      cnrtFree(ctx->src_uv_ptrs_mlu_);
+      ctx->src_uv_ptrs_mlu_ = nullptr;
+    }
+    if (ctx->dst_y_ptrs_mlu_) {
+      cnrtFree(ctx->dst_y_ptrs_mlu_);
+      ctx->dst_y_ptrs_mlu_ = nullptr;
+    }
+    if (ctx->dst_uv_ptrs_mlu_) {
+      cnrtFree(ctx->dst_uv_ptrs_mlu_);
+      ctx->dst_uv_ptrs_mlu_ = nullptr;
+    }
+    ctx->src_yuv_ptrs_cache_.clear();
+    ctx->dst_yuv_ptrs_cache_.clear();
+    if (ctx->queue_) {
+      cnrtDestroyQueue(ctx->queue_);
+    }
+  }
+  return 0;
+}
