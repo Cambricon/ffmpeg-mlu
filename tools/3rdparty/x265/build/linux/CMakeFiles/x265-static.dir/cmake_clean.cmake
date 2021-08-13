4cb7cc80c669ce7c3f2ec15d0559dc0266027442
fanhairui <fanhairui@cambricon.com>
2021-08-13 17:09:04 +0800

    release-1.4.0 official.

--- tools/3rdparty/x265/build/linux/CMakeFiles/x265-static.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libx265.pdb"
-  "libx265.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang ASM_NASM CXX)
-  include(CMakeFiles/x265-static.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
