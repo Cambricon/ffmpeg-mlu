29cdbf1f09a55dd353b8327f9ea612a9b5c7051e
fanhairui <fanhairui@cambricon.com>
2022-05-09 15:41:03 +0800

    [release] mlu v2.3.0

--- compile_ffmpeg.sh
+++ compile_ffmpeg.sh
@@ -1,5 +1,53 @@
 #!/usr/bin/env bash
 
+PARAM_NUM=$#
+
+function compile_script_helper() {
+   echo ""
+   echo "***** ffmpeg compile script instructions *****"
+   echo " ./compile_ffmpeg.sh <mlu_platform> <neuware_home>"
+   echo "    <mlu_platform>: Required. Choose from MLU200 or MLU370, depending on your situation"
+   echo "    <neuware_home>: Optional. if you have set \$NEUWARE_HOME in your environment variable, it can be null."
+   echo "                    Otherwise the default neuware home path is /usr/local/neuware"
+   echo ""
+   echo "    e.g. if your hardware platform is MLU200, use this:"
+   echo "      ./compile_ffmpeg.sh MLU200"
+   echo "    e.g. if your hardware platform is MLU370 and neuware path is /tmp/neuware, use this:"
+   echo "      ./compile_ffmpeg.sh MLU370 /tmp/neuware"
+   echo "**********************************************"
+   echo ""
+}
+
+if [ ${PARAM_NUM} -ne 1 ] && [ ${PARAM_NUM} -ne 2 ];then
+   compile_script_helper
+   exit 0
+fi
+
+NEUWARE_HOME_PATH=""
+if [ -z ${NEUWARE_HOME} ]; then
+   NEUWARE_HOME_PATH="/usr/local/neuware"
+else
+   NEUWARE_HOME_PATH=${NEUWARE_HOME}
+fi
+
+MLU_PLATFORM="$1"
+if [ ${PARAM_NUM} -eq 2 ]; then
+   NEUWARE_HOME_PATH=$2
+fi
+
+FFMPEG_MLU_VER=""
+FFMPEG_MLU_LIB_VER=""
+if [ "${MLU_PLATFORM}" == "MLU370" ]; then
+   FFMPEG_MLU_VER="300"
+   FFMPEG_MLU_LIB_VER="_v3"
+elif [ "${MLU_PLATFORM}" == "MLU200" ]; then
+   FFMPEG_MLU_VER="200"
+else
+   echo "!!! ERROR: The input parameter is not supported: ${MLU_PLATFORM} !!!"
+   compile_script_helper
+   exit 0
+fi
+
 # ----------------------get FFmpeg (ver.4.2)---------------------- #
 if [ -d "./ffmpeg-mlu/" ];then
    echo "ffmpeg-mlu directory exit ..."
@@ -9,74 +57,37 @@ else
    mv ffmpeg ffmpeg-mlu
 fi
 
-PROJ_DIR=${PWD}
 # ------------------------compile ffmpeg-mlu---------------------- #
-# NOTICE, here you can adjust the FFmpeg building configuration parameters
-# For example, if you decided not to use VAAPI (video acceleration API) use --disable-vaapi.
-# Check out available flags on the official FFmpeg website.
-cd ffmpeg-mlu && \
+cd ffmpeg-mlu && rm -rf build &&\
 git checkout -f && git clean -f && \
-git apply ../ffmpeg4.2_mlu.patch
+git apply ../ffmpeg4.2_mlu${FFMPEG_MLU_VER}.patch
 
-FFMPEG_LIB_DIR=""${PWD}"/build"
-mkdir -pv "${FFMPEG_LIB_DIR}"
+PROJ_DIR=${PWD}
+FFMPEG_BUILD_DIR=""${PROJ_DIR}"/build"
+mkdir -pv "${FFMPEG_BUILD_DIR}"
 
-cd $FFMPEG_LIB_DIR
+cd $FFMPEG_BUILD_DIR
 
 ../configure \
-  --prefix="${FFMPEG_LIB_DIR}" \
+  --prefix="${FFMPEG_BUILD_DIR}" \
   --extra-libs="-lpthread -lm" \
-  --extra-cflags="-I/usr/local/neuware/include" \
-  --extra-ldflags="-L/usr/local/neuware/lib64" \
-  --extra-libs="-lcnrt -lcncodec -lcndrv -ldl" \
+  --extra-cflags="-I${NEUWARE_HOME_PATH}/include" \
+  --extra-ldflags="-L${NEUWARE_HOME_PATH}/lib64" \
+  --extra-libs="-lcnrt -lcncodec${FFMPEG_MLU_LIB_VER} -lcndrv -ldl" \
   --enable-ffplay \
   --enable-ffmpeg \
   --enable-mlumpp \
   --enable-gpl \
   --enable-version3 \
   --enable-nonfree \
-  --enable-libfreetype \
   --enable-shared \
   --disable-static \
   --enable-stripping \
   --enable-optimizations
-  # --cpu=native \
-  # --enable-libx264 \
-  # --enable-libx265 \
-  # --enable-debug=3 \
-  # --disable-debug \
-  # --disable-vaapi
 
 make -j$(nproc) && \
-make -j$(nproc) install && \
-make -j$(nproc) distclean
-# ------------------------prepare for rel---------------------- #
-rm -rf ../../build
-mv -f ${FFMPEG_LIB_DIR} ../../
+make -j$(nproc) examples && \
+make -j$(nproc) install
 
-cd ${PROJ_DIR}
-SAMPLES_DIR=""${PROJ_DIR}"/samples/"
-
-function clean_soft_link(){
-   for file in `ls $1`
-   do
-      echo "clean: $1${file}"
-      rm -rf $1${file}/3rdparty/ffmpeg
-      rm -rf $1${file}/3rdparty/ffmpeg
-   done
-}
-
-function get_samples_file(){
-   for file in `ls $1`
-   do
-      echo "create ln -s $1${file}"
-      mkdir -p $1${file}/3rdparty/ffmpeg
-      ln -s ${PROJ_DIR}/build/include $1${file}/3rdparty/ffmpeg/include
-      ln -s ${PROJ_DIR}/build/lib $1${file}/3rdparty/ffmpeg/lib
-   done
-}
-clean_soft_link ${SAMPLES_DIR}
-get_samples_file ${SAMPLES_DIR}
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${PROJ_DIR}/build/lib
-echo "*********compile and prepair ready*********"
-echo
+echo "******** compile and preparations are ready ******** "
+echo "******** the compiled files are in the ffmpeg-mlu folder ******** "
