31bffbd6194d1feb51628955ee7f3cdc989bc421
fanhairui <fanhairui@cambricon.com>
2024-07-26 17:22:57 +0800

    [release] v4.0.0

--- compile_ffmpeg.sh
+++ compile_ffmpeg.sh
@@ -1,79 +1,87 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-PARAM_NUM=$#
-
-function compile_script_helper() {
-   echo ""
-   echo "***** ffmpeg compile script instructions *****"
-   echo " ./compile_ffmpeg.sh <mlu_platform> <bangware_home>"
-   echo "    <mlu_platform>: Required. Choose from MLU200 or MLU370, depending on your situation"
-   echo "    <bangware_home>: Optional. if you have set \$BANGWARE_HOME in your environment variable, it can be null."
-   echo "                    Otherwise the default bangware home path is /usr/local/neuware"
-   echo ""
-   echo "    e.g. if your hardware platform is MLU200, use this:"
-   echo "      ./compile_ffmpeg.sh MLU200"
-   echo "    e.g. if your hardware platform is MLU370 and bangware path is /tmp/bangware, use this:"
-   echo "      ./compile_ffmpeg.sh MLU370 /tmp/bangware"
-   echo "**********************************************"
-   echo ""
+print_usage() {
+    echo ""
+    echo "****** FFMpeg-MLU compile instructions ******"
+    echo " export BANGWARE_HOME=/x/xxx"
+    echo " ./compile_ffmpeg.sh <mlu_platform> <base_version>"
+    echo "    <mlu_platform>: Required, set MLU300、MLU370 or MLU500. default MLU300."
+    echo "    <base_version>: Optional, set 4.4 or 4.2. default 4.4."
+    echo ""
+    echo "    e.g. usage:"
+    echo "        ./compile_ffmpeg.sh MLU300"
+    echo "     or ./compile_ffmpeg.sh MLU370 4.2"
+    echo "     or ./compile_ffmpeg.sh MLU500 4.4"
+    echo "*****************************************************************************"
+    echo ""
 }
 
-if [ ${PARAM_NUM} -ne 1 ] && [ ${PARAM_NUM} -ne 2 ];then
-   compile_script_helper
-   exit 0
+if [ $# -ne 1 ] && [ $# -ne 2 ]; then
+    print_usage
+    exit -1
 fi
 
-BANGWARE_HOME_PATH=""
+BANGWARE_HOME=""
 if [ -z ${BANGWARE_HOME} ]; then
-   BANGWARE_HOME_PATH="/usr/local/neuware"
+    BANGWARE_HOME="/usr/local/neuware"
 else
-   BANGWARE_HOME_PATH=${BANGWARE_HOME}
+    BANGWARE_HOME=${BANGWARE_HOME}
 fi
 
-MLU_PLATFORM="$1"
-if [ ${PARAM_NUM} -eq 2 ]; then
-   BANGWARE_HOME_PATH=$2
-fi
-
-FFMPEG_MLU_VER=""
-FFMPEG_MLU_LIB_VER=""
-if [[ "${MLU_PLATFORM}" == "MLU370" || "${MLU_PLATFORM}" == "MLU300" || "${MLU_PLATFORM}" == "MLU500" ]];then
-   FFMPEG_MLU_VER="300"
-   FFMPEG_MLU_LIB_VER="_v3"
-elif [[ "${MLU_PLATFORM}" == "MLU200" || "${MLU_PLATFORM}" == "MLU220" || "${MLU_PLATFORM}" == "MLU270" ]];then
-   FFMPEG_MLU_VER="200"
+MLU_PLATFORM=""
+if [ -z $1 ]; then
+    MLU_PLATFORM="MLU370"
 else
-   echo "!!! ERROR: The input parameter is not supported: ${MLU_PLATFORM} !!!"
-   compile_script_helper
-   exit 0
+    MLU_PLATFORM=$1
+fi
+if [[ "${MLU_PLATFORM}" != "MLU370" && "${MLU_PLATFORM}" != "MLU300" &&
+      "${MLU_PLATFORM}" != "MLU500" ]]; then
+    echo ""
+    echo "!!! ERROR: The input parameter is not supported: ${MLU_PLATFORM} !!!"
+    print_usage
+    exit -1
 fi
 
-# ----------------------get FFmpeg (ver.4.2)---------------------- #
-if [ -d "./ffmpeg-mlu/" ];then
+BASE_VERSION=""
+if [ -z $2 ]; then
+    BASE_VERSION="4.4"
+else
+    BASE_VERSION=$2
+fi
+
+DST_BRANCH=""
+if [[ "${BASE_VERSION}" == "4.4" ]]; then
+    DST_BRANCH="n4.4.4"
+else
+    DST_BRANCH="release/4.2"
+fi
+
+# ------------------------------------------------------------
+# FFMpeg base branch
+if [ -d "./ffmpeg-mlu" ]; then
    echo "ffmpeg-mlu directory exit ..."
 else
    echo "ffmpeg-mlu directory not exit, now download ..."
-   git clone https://gitee.com/mirrors/ffmpeg.git -b release/4.2 --depth=1
+   git clone https://gitee.com/mirrors/ffmpeg.git -b ${DST_BRANCH} --depth=1
    mv ffmpeg ffmpeg-mlu
 fi
 
-# ------------------------compile ffmpeg-mlu---------------------- #
-cd ffmpeg-mlu && rm -rf build &&\
+# ------------------------------------------------------------
+# Make FFMpeg-MLU and compile FFMpeg-MLU
+cd ffmpeg-mlu && rm -rf build &&   \
 git checkout -f && git clean -f && \
-git apply ../ffmpeg4.2_mlu${FFMPEG_MLU_VER}.patch
+git apply ../ffmpeg${BASE_VERSION}_mlu300.patch
 
 PROJ_DIR=${PWD}
 FFMPEG_BUILD_DIR=""${PROJ_DIR}"/build"
-mkdir -pv "${FFMPEG_BUILD_DIR}"
-
-cd $FFMPEG_BUILD_DIR
+mkdir -pv "${FFMPEG_BUILD_DIR}" && cd $FFMPEG_BUILD_DIR
 
 ../configure \
   --prefix="${FFMPEG_BUILD_DIR}" \
   --extra-libs="-lpthread -lm" \
-  --extra-cflags="-I${BANGWARE_HOME_PATH}/include" \
-  --extra-ldflags="-L${BANGWARE_HOME_PATH}/lib64" \
-  --extra-libs="-lcnrt -lcncodec${FFMPEG_MLU_LIB_VER} -lcndrv -ldl" \
+  --extra-cflags="-I${BANGWARE_HOME}/include" \
+  --extra-ldflags="-L${BANGWARE_HOME}/lib64" \
+  --extra-libs="-lcnrt -lcncodec_v3 -lcndrv -ldl" \
   --enable-ffplay \
   --enable-ffmpeg \
   --enable-mlu \
@@ -88,9 +96,12 @@ cd $FFMPEG_BUILD_DIR
   --disable-x86asm \
   --enable-optimizations
 
-make -j$(nproc) && \
-make -j$(nproc) examples && \
+make -j$(nproc) && make -j$(nproc) examples && \
 make -j$(nproc) install
 
-echo "******** compile and preparations are ready ******** "
-echo "******** the compiled files are in the ffmpeg-mlu folder ******** "
+echo ""
+echo "*****************************************************************************"
+echo "compile and preparations are ready"
+echo "the compiled files are in the ffmpeg-mlu folder"
+echo "*****************************************************************************"
+echo ""
