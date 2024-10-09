3951e69ba328776f19697eb5dff4ca84c7dee89c
fanhairui <fanhairui@cambricon.com>
2024-10-09 11:27:52 +0800

    [release] new v4.1.1

--- env.sh
+++ env.sh
@@ -3,9 +3,7 @@ CUR_DIR=$(dirname $(readlink -f "${BASH_SOURCE[0]}") )
 
 if [ -z ${NEUWARE_HOME} ]; then
     echo "Error: not set NEUWARE_HOME environment variable"
-    exit 1
-else
-    NEUWARE_HOME=${NEUWARE_HOME}
+    exit
 fi
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${CUR_DIR}/install/lib:${NEUWARE_HOME}/lib64
